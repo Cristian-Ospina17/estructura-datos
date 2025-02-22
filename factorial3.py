82c296d3c3adbf2dee68b8876030401c24798600
Cristian-Ospina17 <cristianospina882@gmail.com>
2025-02-22 17:11:07 +0000

    actividad

--- factorial3.py
+++ factorial3.py
@@ -1,10 +1,8 @@
 numero = 5
-resultado: int
 
 def factorial (n:int)->int :
   res:int = 1
-  for i in range (1, n + 1):
-      res= res * i 
-  return res
-
+  if n == 1:
+     return n
+  return factorial (n-1) * n
 print(factorial(numero))
